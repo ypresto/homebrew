ce4cc84c9fb4e3e02e893f4dd13581f4d46ee5a4
James Gifford <james@jamesrgifford.com>
2012-05-17 09:58:23 -0400

    unrar formula works on Ubuntu

--- Library/Formula/unrar.rb
+++ Library/Formula/unrar.rb
@@ -4,6 +4,7 @@ class Unrar < Formula
   url 'http://www.rarlab.com/rar/unrarsrc-4.1.4.tar.gz'
   sha1 'ae4b1e2c99e96527c4a97f980daa547499f42a0f'
   homepage 'http://www.rarlab.com'
+  platforms :mac, :linux
 
   def install
     system "make --makefile makefile.unix"
