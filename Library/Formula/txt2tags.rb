032356d774782f8d64439dc7119cdf397a1689eb
James Gifford <james@jamesrgifford.com>
2012-05-17 09:58:35 -0400

    txt2tags works on ubuntu

--- Library/Formula/txt2tags.rb
+++ Library/Formula/txt2tags.rb
@@ -4,6 +4,7 @@ class Txt2tags < Formula
   url 'http://txt2tags.googlecode.com/files/txt2tags-2.6.tgz'
   homepage 'http://txt2tags.org'
   md5 'ac09fd624f1e3a553d5f0e01271cc0ee'
+  platforms :mac, :linux
 
   def install
     bin.install 'txt2tags'
