29377d03114ca8dccbde8dfd07e16688f1dc67ad
James Gifford <james@jamesrgifford.com>
2012-05-17 10:05:26 -0400

    mad works on ubuntu

--- Library/Formula/mad.rb
+++ Library/Formula/mad.rb
@@ -4,7 +4,7 @@ class Mad < Formula
   homepage 'http://www.underbit.com/products/mad/'
   url 'http://downloads.sourceforge.net/project/mad/libmad/0.15.1b/libmad-0.15.1b.tar.gz'
   md5 '1be543bc30c56fb6bea1d7bf6a64e66c'
-
+  platforms :mac, :linux
   def install
     fpm = MacOS.prefer_64_bit? ? '64bit': 'intel'
     system "./configure", "--disable-debugging", "--enable-fpm=#{fpm}", "--prefix=#{prefix}"
