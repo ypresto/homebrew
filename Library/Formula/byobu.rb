c49dcf8091d07472f8e14c6c6cb1b453982b4e77
James Gifford <james@jamesrgifford.com>
2012-05-17 10:03:09 -0400

    byobu works on ubuntu

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -4,7 +4,7 @@ class Byobu < Formula
   url 'http://launchpad.net/byobu/trunk/5.12/+download/byobu_5.12.orig.tar.gz'
   homepage 'http://launchpad.net/byobu'
   md5 '15c3d0fd2923fba0a6d77fc52d59b81a'
-
+  platforms :mac, :linux 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
