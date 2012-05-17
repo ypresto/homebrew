5bd9b325b83859c7e7821a2b70b6d833f079d099
James Gifford <james@jamesrgifford.com>
2012-05-17 10:10:56 -0400

    fasd works on ubuntu

--- Library/Formula/fasd.rb
+++ Library/Formula/fasd.rb
@@ -4,7 +4,7 @@ class Fasd < Formula
   homepage 'https://github.com/clvv/fasd'
   url 'https://github.com/clvv/fasd/tarball/0.5.4'
   md5 '9f051346463f19771d64c6a31a85a22b'
-
+  platforms :mac, :linux
   def install
     bin.install 'fasd'
     man1.install 'fasd.1'
