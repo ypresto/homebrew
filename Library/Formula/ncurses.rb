a77b34fdd1c42f5fdc9a031a8f37f599a7f401f5
Yuya Tanaka <yuya.presto@gmail.com>
2012-05-22 15:54:59 +0900

    Added missing ncurses.rb and depends_on it

---
+++ Library/Formula/ncurses.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+# imported from homebrew-dupes
+
+class Ncurses < Formula
+  homepage 'http://www.gnu.org/s/ncurses/'
+  url 'http://ftpmirror.gnu.org/ncurses/ncurses-5.9.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz'
+  sha1 '3e042e5f2c7223bffdaac9646a533b8c758b65b5'
+  platforms :linux
+
+  def options
+    [['--universal', 'Build for both 32 & 64 bit Intel.']]
+  end
+
+  def install
+    ENV.universal_binary if ARGV.build_universal?
+
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--with-shared",
+                          "--with-widec",
+                          "--with-manpage-format=normal",
+                          "--enable-symlinks"
+    system "make"
+    system "make install"
+  end
+end
