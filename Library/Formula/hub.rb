853f8b4c7ac6a3272b27a38c4e0d43b89af6be43
Yuya Tanaka <yuya.presto@gmail.com>
2012-08-30 13:40:14 +0900

    Imported latest hub.rb

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,12 +1,39 @@
 require 'formula'
 
 class Hub < Formula
-  url 'https://github.com/defunkt/hub/tarball/v1.8.3'
-  homepage 'https://github.com/defunkt/hub#readme'
+  url 'https://github.com/defunkt/hub/tarball/v1.10.2'
+  homepage 'http://defunkt.io/hub/'
   head 'https://github.com/defunkt/hub.git'
-  md5 '8775c2312ba2b175775a81e9b0bf5e24'
+  md5 '12357b7542d1caf4ec049343e8564057'
+  platforms :mac, :linux
+
+  @_mac = mac
 
   def install
-    system "rake", "install", "prefix=#{prefix}"
+    system rake_bin, "install", "prefix=#{prefix}"
+    (prefix+'etc/bash_completion.d').install 'etc/hub.bash_completion.sh'
+    (share+'zsh/site-functions').install 'etc/hub.zsh_completion' => '_hub'
+  end
+
+  def rake_bin
+    require 'rbconfig'
+    ruby_rake = File.join RbConfig::CONFIG['bindir'], 'rake'
+
+    if File.exist? ruby_rake
+      ruby_rake
+    elsif @_mac
+      '/usr/bin/rake'
+    else
+      'rake'
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    Bash completion has been installed to:
+      #{etc}/bash_completion.d
+
+    zsh completion has been installed to:
+      #{HOMEBREW_PREFIX}/share/zsh/site-functions
+    EOS
   end
 end
